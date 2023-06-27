c48c25dad36177c0d8c6c4ac095fc1625e1ff8db
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-06-27 09:35:40 -0300

    finished lesson 2

---
+++ projeto-exemplo/src/resources/views/includes/form.blade.php
@@ -0,0 +1,34 @@
+{{-- <div class="row">
+    <div class="col-6 col-md-6">
+        <h3>Sign Up</h3>
+        <form action="#" method="POST">
+            <div class="form-group">
+                <label for="email">Your Email</label>
+                <input type="email" name="email" id="email" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="text">First Name</label>
+                <input type="text" name="first_name" id="text" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" class="form-control">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+    <div class="col-6 col-md-6">
+        <h3>Sign In</h3>
+        <form action="#" method="POST">
+            <div class="form-group">
+                <label for="email">Your Email</label>
+                <input type="email" name="email" id="email" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" class="form-control">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</div> --}}
--- projeto-exemplo/src/resources/views/layouts/master.blade.php
+++ projeto-exemplo/src/resources/views/layouts/master.blade.php
@@ -5,9 +5,22 @@
     <!-- 'yield', com @, faz parte da sintaxe de 'hooks' do laravel -->
     <!-- 'yield' é um 'placeholder', quer dizer 'insira algo aqui, neste local' -->
     <title>@yield('title')</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js"
+        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous">
+    </script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css"
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
 </head>
 
 <body>
+
+    <header>
+        @include('includes.header')
+    </header>
+    <!-- é quase como 1 component, inserido nesse local... no caso, é o nosso header... -->
+
     <div class="container">
         @yield('content')
     </div>
